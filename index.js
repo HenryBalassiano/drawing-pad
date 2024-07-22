d1b259354472b65ba9c71a56990e6026fd130f8f
Henry Balassiano <henrybalassiano@gmail.com>
2024-07-22 01:35:14 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,59 @@
+let gridSize = 16;
+let isDrawing = false;
+
+function createGrid() {
+  let container = document.getElementById("main-wrapper");
+  container.innerHTML = ""; // Clear previous grid
+  for (let i = 0; i < gridSize; i++) {
+    let col = document.createElement("div");
+    col.className = "col";
+
+    for (let j = 0; j < gridSize; j++) {
+      let row = document.createElement("div");
+      row.className = "row";
+      col.appendChild(row);
+    }
+    document.getElementById(
+      "slider-label"
+    ).innerHTML = `Grid Size: ${gridSize}`;
+
+    container.appendChild(col);
+  }
+}
+
+function draw() {
+  const elements = document.querySelectorAll(".row");
+
+  elements.forEach((element) => {
+    element.addEventListener("mousedown", function () {
+      isDrawing = true;
+      this.style.backgroundColor = "yellow";
+    });
+    element.addEventListener("mousemove", function () {
+      if (isDrawing) {
+        this.style.backgroundColor = "yellow";
+      }
+    });
+  });
+
+  document.addEventListener("mouseup", function () {
+    isDrawing = false;
+  });
+}
+
+function changeGrid() {
+  var slider = document.getElementById("myRange");
+  slider.addEventListener("input", function () {
+    gridSize = this.value;
+    createGrid();
+    draw();
+  });
+}
+
+function init() {
+  createGrid();
+  changeGrid();
+  draw();
+}
+
+init();
